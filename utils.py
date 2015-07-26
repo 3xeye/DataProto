99c0ea154fd9cb28c443d72d9c9cdf4c04077740
M.L <3xeye@163.com>
2015-07-26 22:57:40 +0800

    0.002

--- utils.py
+++ utils.py
@@ -7,10 +7,14 @@ Created on 2015Äê7ÔÂ26ÈÕ
 '''
 import skill_general_data as s
 keys = set([])
+keys1 = set([])
 for k,v in s.data.iteritems():
     if type(v) !=dict:
         print k,'-',v
     for k1,v1 in v.iteritems():
         if type(v1) != int:
-            print k, v1, type(v1)
+            keys1.add((k1,type(v1)))
+            #print k, k1, type(v1)
 print keys
+for k in keys1:
+    print k
